5652468d8cb59c0d0768437bd717783b1a881ba7
Leonardo Vizcaya Savchenko <lvizcaya@arpixnet.com>
2026-02-11 15:19:55 -0500

    Create dashboard.vue

---
+++ app/pages/dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: 'auth',
+  requiresAuth: true
+})
+</script>
+
+<template>
+  <div>
+    <UContainer class="p-4">
+      <h1>Dashboard</h1>
+    </UContainer>
+  </div>
+</template>
